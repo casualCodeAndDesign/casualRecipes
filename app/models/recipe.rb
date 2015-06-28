75de7865c1c54e728083334d751a2d68d46614bf
Joni Kämppä <johnniecasual@gmail.com>
2015-06-28 12:18:39 +0000

    add imageuploader

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,13 @@ class Recipe < ActiveRecord::Base
   validates :name, presence: true, length: { in: 5..100 }
   validates :summary, presence: true, length: { in: 10..150 }
   validates :description, presence: true, length: { in: 20..500 }
+  mount_uploader :picture, PictureUploader
+  validate :picture_size
+  
+  private
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "Should be less than 5mb")
+      end
+    end
 end
