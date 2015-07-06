e73e3708440e409d06dc282f644d881c9aad3730
Joni Kämppä <johnniecasual@gmail.com>
2015-07-06 07:02:21 +0000

    Added admin functionality to recipe destroy and edit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150704144350) do
+ActiveRecord::Schema.define(version: 20150706063257) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150704144350) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.datetime "last_login"
+    t.boolean  "admin",           default: false
   end
 
   create_table "ingredients", force: :cascade do |t|
